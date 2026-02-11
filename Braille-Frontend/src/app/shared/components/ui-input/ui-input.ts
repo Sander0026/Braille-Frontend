ef87a28a89e7e0ea8950f7ee47ba0329dd3d561e
alexsander ribeiro <102040731+Sander0026@users.noreply.github.com>
2026-02-11 09:20:34 -0300

    Funcionalidade: Inicialização do aplicativo Braille-Frontend com componentes principais e roteamento
    - Adicionada configuração de roteamento do aplicativo.
    
    - Criado o componente principal do aplicativo com o sinal de título.
    
    - Implementados os componentes principais: Cabeçalho, Rodapé e Barra Lateral.
    
    - Desenvolvidos os componentes compartilhados: UiCard, UiInput e UiModal.
    
    - Configurados os serviços de acessibilidade e autenticação.
    
    - Configurado o Tailwind CSS para estilização.
    
    - Estabelecido o framework de testes com testes iniciais para componentes e serviços.
    
    - Criados os arquivos index.html e main.ts para inicialização do aplicativo.
    
    - Adicionados arquivos de configuração TypeScript para o aplicativo e o spec.

---
+++ Braille-Frontend/src/app/shared/components/ui-input/ui-input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-input',
+  imports: [],
+  templateUrl: './ui-input.html',
+  styleUrl: './ui-input.css',
+})
+export class UiInput {
+
+}
