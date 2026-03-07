ac74c00db0288a85cd09d730d5f1a95075722a7f
alexsander ribeiro <102040731+Sander0026@users.noreply.github.com>
2026-03-07 19:20:39 -0300

    feat(pwa): Angular Installer PWA Fase 22 Offline SW

--- Braille-Frontend/src/app/app.config.ts
+++ Braille-Frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, ErrorHandler } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, ErrorHandler, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -7,21 +7,23 @@ import * as Sentry from '@sentry/angular';
 import { routes } from './app.routes';
 import { apiInterceptor } from './core/interceptors/api.interceptor';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     {
-      provide: ErrorHandler,
-      useValue: Sentry.createErrorHandler({
-        showDialog: false,
-      }),
+        provide: ErrorHandler,
+        useValue: Sentry.createErrorHandler({
+            showDialog: false,
+        }),
     },
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([apiInterceptor, authInterceptor])
-    )
-  ]
+    provideHttpClient(withFetch(), withInterceptors([apiInterceptor, authInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
